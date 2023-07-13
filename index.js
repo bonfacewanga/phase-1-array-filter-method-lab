559d077da665d4507778666c287785bda63bba86
Bonface <bonniewanga34@gmail.com>
2023-07-13 13:05:09 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+// Find all drivers that match the passed-in name
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+ }
+  
+   //Find drivers whose names start with the provided letters
+   //The startsWith() method determines whether a string begins with the characters of a specified string, returning true or false as appropriate.
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
+  }
+  
+  // Find drivers whose name property matches the given name
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
